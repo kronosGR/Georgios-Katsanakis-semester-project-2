f9bf0dc932ec26b3a9af31adc9684f94eda94a24
kronosGR <geo.elgeo@gmail.com>
2021-11-24 12:19:23 +0100

    account responsive

--- js/components/account_product.js
+++ js/components/account_product.js
@@ -3,7 +3,7 @@ import EditProduct from './account_product_edit.js'
 
 export default function (item) {
   return `
-    <div log class="d-flex flex-nowrap align-items-center col-12 product-item" href="product-details.html?id=${item.id}">
+    <div log class="d-flex flex-nowrap align-items-center col-12 product-item justify-content-center href="product-details.html?id=${item.id}">
       <img class="col-1 product-item__img" src="${item.image_url}" alt="${item.title}"/>
       <span class="col-5 product-item__title">${item.title}</span> 
       <span class="col-1 product-item__price">$${item.price.toFixed(2)}</span>
