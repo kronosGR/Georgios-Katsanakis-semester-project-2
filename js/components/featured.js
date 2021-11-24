fa4c172b0b5a4c819f751081db7ebc100002c32b
kronosGR <geo.elgeo@gmail.com>
2021-11-24 11:01:15 +0100

    Index responsive

--- js/components/featured.js
+++ js/components/featured.js
@@ -5,16 +5,19 @@ const cont = document.querySelector('.cont');
 
 export default async function () {
   const featuredList = await Strapi.getFeatured();
-  
+
   let el = `
-    <div class="cont__featured">
+    <div class="cont__featured ">
       <h2>Featured</h2>
     </div>
-  `
+    <div class="d-flex flex-column flex-md-row flex-md-wrap justify-content-center">
+  `;
 
-  featuredList.forEach(item => {
+  featuredList.forEach((item) => {
     el += cart_item(item);
-  })
+  });
+
+  el += '</div>';
 
   cont.innerHTML = el;
 }
